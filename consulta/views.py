493eed1ff188fe4ca49487d9192857081d2984c8
gonzard <g.zardain@codelab.mx>
2016-06-29 14:35:45 -0500

    views .1

--- consulta/views.py
+++ consulta/views.py
@@ -3,42 +3,26 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 
-def tarento_home(request):
-	#Products = Producto.objects.order_by('id').reverse()[:8]
-	Categorias = Categoria.objects.order_by('?')[:1]
-	seccion1 = Seccion1.objects.order_by('titulo')[:3]
-	seccion2 = Seccion2.objects.order_by('?')[:1]
-	seccion3 = Seccion3.objects.order_by('?')[:1]
-	Slides = Diapositivas.objects.order_by('prioridad')[:6]
-	Galerias = Galeria.objects.reverse()[:6]
-	Active = 'home'
+def consult_home(request):
 	return render(request, 'index.html')
 
-def tarento_nosotros(request):
-	Active = 'nosotros'
-	About = Nosotros.objects.reverse()[:1]
-	Galerias = Galeria.objects.reverse()[:6]
-	return render(request, 'nosotros.html', {'Active': Active, 'Galerias': Galerias, 'About': About})
+def consult_nosotros(request):
+	return render(request, 'nosotros.html')
 
-def tarento_contacto(request):
-	return render(request, 'contacto.html', {'Categorias': Categorias, 'form': form_class, 'Active': Active, 'Galerias': Galerias, 'Message': Message})
+def consult_servicios(request):
+	return render(request, 'servicios.html')
 
-def tarento_privacy(request):
-	Galerias = Galeria.objects.reverse()[:6]
-	Policy = Privacidad.objects.reverse()[:1]
-	return render(request, 'privacy.html', {'Galerias': Galerias, 'Policy': Policy})
+def consult_soluciones(request):
+	return render(request, 'soluciones.html')
 
+def consult_clientes(request):
+	return render(request, 'clientes.html')
 
+def consult_contacto(request):
+	return render(request, 'contacto.html')
 
-def consult_privacy(request):
-	Galerias = Galeria.objects.reverse()[:6]
-	Policy = Privacidad.objects.reverse()[:1]
-	return render(request, 'privacy.html', {'Galerias': Galerias, 'Policy': Policy})
+def consult_faq(request):
+	return render(request, 'faq.html')
 
 def consult_sitemap(request):
-	Catalogos = Catalogo.objects.order_by('id').reverse()
-	Policy = Privacidad.objects.reverse()[:1]
-	Categorias = Categoria.objects.order_by('id')
-	Products = Producto.objects.order_by('id')
-	Line = Linea.objects.order_by('id')
-	return render(request, 'sitemap.xml', {'Catalogos': Catalogos, 'Policy': Policy, 'Categorias': Categorias, 'Products': Products, 'Line': Line}, content_type='text/xml')
+	return render(request, 'sitemap.xml', content_type = "text/xml")
