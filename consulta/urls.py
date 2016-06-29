e61b450203923162a4c7dd3caf345973b7a59546
ecruz91 <e.cruz@codelab.mx>
2016-06-29 14:17:05 -0500

    Urls.py added

--- consulta/urls.py
+++ consulta/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.consult_home),
+	url(r'^nosotros\.html/$', views.consult_nosotros),
+    url(r'^servicios\.html/', views.consult_servicios),
+    url(r'^soluciones\.html/', views.consult_soluciones),
+    url(r'^clientes\.html/$', views.consult_clientes),
+    url(r'^contacto\.html/$', views.consult_contacto),
+    url(r'^faq\.html/$', views.consult_faq),
+    url(r'^sitemap\.xml$', views.consult_sitemap),
 ]
